1eac59dd1191e71a91e76b00167bab08bbd6c861
ha-nagisa <nagisa18222@icloud.com>
2021-11-17 14:37:49 +0900

    Finished before login layouts

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,13 +1,12 @@
 <template>
   <user-form-card>
     <template #user-form-card-content>
-      <v-form v-model="isValid">
+      <v-form ref="form" v-model="isValid">
         <user-form-name :name.sync="params.user.name" />
         <user-form-email :email.sync="params.user.email" placeholder />
         <user-form-password :password.sync="params.user.password" set-validation />
-        <v-btn :disabled="!isValid" block color="appblue" class="white--text">登録する</v-btn>
+        <v-btn :disabled="!isValid || loading" :loading="loading" block color="appblue" class="white--text" @click="signup">登録する</v-btn>
       </v-form>
-      {{ params }}
     </template>
   </user-form-card>
 </template>
@@ -18,8 +17,22 @@ export default {
   data() {
     return {
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } },
     }
+  },
+  methods: {
+    signup() {
+      this.loading = true
+      this.formReset()
+      setTimeout(() => (this.loading = false), 1500)
+    },
+    formReset() {
+      this.$refs.form.reset()
+      for (const key in this.params.user) {
+        this.params.user[key] = ''
+      }
+    },
   },
 }
 </script>
